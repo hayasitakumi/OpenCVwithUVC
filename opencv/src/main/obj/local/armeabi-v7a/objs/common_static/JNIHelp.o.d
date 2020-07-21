2bb1d1a22d864dcec01812a10b18c858a117a084
hayasitakumi <b1701571@planet.kanazawa-it.ac.jp>
2020-07-21 13:05:55 +0900

    gradle -> 4.0.1

---
+++ opencv/src/main/obj/local/armeabi-v7a/objs/common_static/JNIHelp.o.d
@@ -0,0 +1,15 @@
+./obj/local/armeabi-v7a/objs/common_static/JNIHelp.o: \
+  jni/common/JNIHelp.cpp jni/common/../utilbase.h \
+  jni/common/../localdefines.h jni/common/JniConstants.h \
+  jni/common/JNIHelp.h \
+  /Users/syabonbubble/Library/Android/sdk/ndk/16.1.4479499/sources/cxx-stl/gnu-libstdc++/4.9/include/string
+
+jni/common/../utilbase.h:
+
+jni/common/../localdefines.h:
+
+jni/common/JniConstants.h:
+
+jni/common/JNIHelp.h:
+
+/Users/syabonbubble/Library/Android/sdk/ndk/16.1.4479499/sources/cxx-stl/gnu-libstdc++/4.9/include/string:
