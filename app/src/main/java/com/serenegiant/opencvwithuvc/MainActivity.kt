25223177e0742edcbcb0df4d4e121b26f5ec345a
hayasitakumi <b1701571@planet.kanazawa-it.ac.jp>
2020-07-29 14:24:17 +0900

    MainActivityをUVCだけに

--- app/src/main/java/com/serenegiant/opencvwithuvc/MainActivity.kt
+++ app/src/main/java/com/serenegiant/opencvwithuvc/MainActivity.kt
@@ -22,23 +22,13 @@
  */
 package com.serenegiant.opencvwithuvc
 
-import android.animation.Animator
-import android.graphics.Bitmap
-import android.graphics.Matrix
-import android.graphics.Typeface
 import android.hardware.usb.UsbDevice
 import android.os.Bundle
 import android.util.Log
 import android.view.Surface
-import android.view.SurfaceView
 import android.view.View
-import android.view.View.OnLongClickListener
 import android.widget.*
-import android.widget.SeekBar.OnSeekBarChangeListener
 import com.serenegiant.common.BaseActivity
-//import com.serenegiant.opencv.ImageProcessor
-//import com.serenegiant.opencv.ImageProcessor.ImageProcessorCallback
-import com.serenegiant.opencvwithuvc.MainActivity
 import com.serenegiant.usb.CameraDialog
 import com.serenegiant.usb.CameraDialog.CameraDialogParent
 import com.serenegiant.usb.USBMonitor
@@ -46,13 +36,7 @@ import com.serenegiant.usb.USBMonitor.OnDeviceConnectListener
 import com.serenegiant.usb.USBMonitor.UsbControlBlock
 import com.serenegiant.usb.UVCCamera
 import com.serenegiant.usbcameracommon.UVCCameraHandlerMultiSurface
-import com.serenegiant.utils.CpuMonitor
-import com.serenegiant.utils.ViewAnimationHelper
-import com.serenegiant.utils.ViewAnimationHelper.ViewAnimationListener
-import com.serenegiant.widget.UVCCameraTextureView
 import kotlinx.android.synthetic.main.activity_main.*
-import java.nio.ByteBuffer
-import java.util.*
 
 class MainActivity : BaseActivity(), CameraDialogParent {
     /**
@@ -64,26 +48,12 @@ class MainActivity : BaseActivity(), CameraDialogParent {
      * Handler to execute camera related methods sequentially on private thread
      */
     private var mCameraHandler: UVCCameraHandlerMultiSurface? = null
-//    private var mImageProcessor: ImageProcessor? = null
-    private val cpuMonitor = CpuMonitor()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (DEBUG) Log.v(TAG, "onCreate:")
         setContentView(R.layout.activity_main)
         camera_button.setOnCheckedChangeListener(mOnCheckedChangeListener)
-//        capture_button.setOnClickListener(mOnClickListener)
-//        capture_button.visibility = View.INVISIBLE
-        camera_view.setOnLongClickListener(mOnLongClickListener)
-//        brightness_button.setOnClickListener(mOnClickListener)
-//        contrast_button.setOnClickListener(mOnClickListener)
-//        reset_button.setOnClickListener(mOnClickListener)
-//        setting_seekbar.setOnSeekBarChangeListener(mOnSeekBarChangeListener)
-//        tools_layout.visibility = View.INVISIBLE
-//        value_layout.visibility = View.INVISIBLE
-//        cpu_load_textview.typeface = Typeface.MONOSPACE
-//        fps_textview.text = null
-//        fps_textview.typeface = Typeface.MONOSPACE
         mUSBMonitor = USBMonitor(this, mOnDeviceConnectListener)
         mCameraHandler = UVCCameraHandlerMultiSurface.createHandler(this, camera_view,
                 1, PREVIEW_WIDTH, PREVIEW_HEIGHT, PREVIEW_MODE)
@@ -120,27 +90,6 @@ class MainActivity : BaseActivity(), CameraDialogParent {
         super.onDestroy()
     }
 
-    /**
-     * event handler when click camera / capture button
-     */
-    private val mOnClickListener = View.OnClickListener { view ->
-        when (view.id) {
-//            R.id.capture_button -> if (mCameraHandler!!.isOpened) {
-//                if (checkPermissionWriteExternalStorage() && checkPermissionAudio()) {
-//                    if (!mCameraHandler!!.isRecording) {
-//                        capture_button!!.setColorFilter(-0x10000) // turn red
-//                        mCameraHandler!!.startRecording()
-//                    } else {
-//                        capture_button!!.setColorFilter(0) // return to default color
-//                        mCameraHandler!!.stopRecording()
-//                    }
-//                }
-//            }
-//            R.id.brightness_button -> showSettings(UVCCamera.PU_BRIGHTNESS)
-//            R.id.contrast_button -> showSettings(UVCCamera.PU_CONTRAST)
-//            R.id.reset_button -> resetSettings()
-        }
-    }
     private val mOnCheckedChangeListener = CompoundButton.OnCheckedChangeListener { compoundButton, isChecked ->
         when (compoundButton.id) {
             R.id.camera_button -> if (isChecked && !mCameraHandler!!.isOpened) {
@@ -151,35 +100,15 @@ class MainActivity : BaseActivity(), CameraDialogParent {
         }
     }
 
-    /**
-     * capture still image when you long click on preview image(not on buttons)
-     */
-    private val mOnLongClickListener = OnLongClickListener { view ->
-        when (view.id) {
-            R.id.camera_view -> if (mCameraHandler!!.isOpened) {
-                if (checkPermissionWriteExternalStorage()) {
-                    mCameraHandler!!.captureStill()
-                }
-                return@OnLongClickListener true
-            }
-        }
-        false
-    }
-
     private fun setCameraButton(isOn: Boolean) {
         if (DEBUG) Log.v(TAG, "setCameraButton:isOn=$isOn")
         runOnUiThread({
-            if (camera_button != null) {
-                try {
-                    camera_button!!.setOnCheckedChangeListener(null)
-                    camera_button!!.isChecked = isOn
-                } finally {
-                    camera_button!!.setOnCheckedChangeListener(mOnCheckedChangeListener)
-                }
+            try {
+                camera_button.setOnCheckedChangeListener(null)
+                camera_button.isChecked = isOn
+            } finally {
+                camera_button.setOnCheckedChangeListener(mOnCheckedChangeListener)
             }
-//            if (!isOn && capture_button != null) {
-//                capture_button!!.visibility = View.INVISIBLE
-//            }
         }, 0)
         updateItems()
     }
@@ -197,8 +126,6 @@ class MainActivity : BaseActivity(), CameraDialogParent {
                     mPreviewSurfaceId = surface.hashCode()
                     mCameraHandler!!.addSurface(mPreviewSurfaceId, surface, false)
                 }
-//                capture_button!!.visibility = View.VISIBLE
-                startImageProcessor(PREVIEW_WIDTH, PREVIEW_HEIGHT)
             } catch (e: Exception) {
                 Log.w(TAG, e)
             }
@@ -208,7 +135,6 @@ class MainActivity : BaseActivity(), CameraDialogParent {
 
     private fun stopPreview() {
         if (DEBUG) Log.v(TAG, "stopPreview:")
-        stopImageProcessor()
         if (mPreviewSurfaceId != 0) {
             mCameraHandler!!.removeSurface(mPreviewSurfaceId)
             mPreviewSurfaceId = 0
@@ -267,23 +193,7 @@ class MainActivity : BaseActivity(), CameraDialogParent {
 
     //================================================================================
     private val isActive: Boolean
-        private get() = mCameraHandler != null && mCameraHandler!!.isOpened
-
-    private fun checkSupportFlag(flag: Int): Boolean {
-        return mCameraHandler != null && mCameraHandler!!.checkSupportFlag(flag.toLong())
-    }
-
-    private fun getValue(flag: Int): Int {
-        return if (mCameraHandler != null) mCameraHandler!!.getValue(flag) else 0
-    }
-
-    private fun setValue(flag: Int, value: Int): Int {
-        return if (mCameraHandler != null) mCameraHandler!!.setValue(flag, value) else 0
-    }
-
-    private fun resetValue(flag: Int): Int {
-        return if (mCameraHandler != null) mCameraHandler!!.resetValue(flag) else 0
-    }
+        get() = mCameraHandler != null && mCameraHandler!!.isOpened
 
     private fun updateItems() {
         runOnUiThread(mUpdateItemsOnUITask, 100)
@@ -291,252 +201,26 @@ class MainActivity : BaseActivity(), CameraDialogParent {
 
     private val mUpdateItemsOnUITask = Runnable {
         if (isFinishing) return@Runnable
-        val visible_active = if (isActive) View.VISIBLE else View.INVISIBLE
-//        tools_layout!!.visibility = visible_active
-//        brightness_button!!.visibility = if (checkSupportFlag(UVCCamera.PU_BRIGHTNESS)) visible_active else View.INVISIBLE
-//        contrast_button!!.visibility = if (checkSupportFlag(UVCCamera.PU_CONTRAST)) visible_active else View.INVISIBLE
-    }
-    private var mSettingMode = -1
-
-    /**
-     * show setting view
-     * @param mode
-     */
-    private fun showSettings(mode: Int) {
-        if (DEBUG) Log.v(TAG, String.format("showSettings:%08x", mode))
-        hideSetting(false)
-        if (isActive) {
-            when (mode) {
-                UVCCamera.PU_BRIGHTNESS, UVCCamera.PU_CONTRAST -> {
-                    mSettingMode = mode
-//                    setting_seekbar!!.progress = getValue(mode)
-//                    ViewAnimationHelper.fadeIn(value_layout, -1, 0, mViewAnimationListener)
-                }
-            }
-        }
+        if (isActive) View.VISIBLE else View.INVISIBLE
     }
 
-    private fun resetSettings() {
-        if (isActive) {
-//            when (mSettingMode) {
-//                UVCCamera.PU_BRIGHTNESS, UVCCamera.PU_CONTRAST -> setting_seekbar!!.progress = resetValue(mSettingMode)
-//            }
-        }
-        mSettingMode = -1
-//        ViewAnimationHelper.fadeOut(value_layout, -1, 0, mViewAnimationListener)
-    }
-
-    /**
-     * hide setting view
-     * @param fadeOut
-     */
-    protected fun hideSetting(fadeOut: Boolean) {
-        removeFromUiThread(mSettingHideTask)
-        if (fadeOut) {
-//            runOnUiThread({ ViewAnimationHelper.fadeOut(value_layout, -1, 0, mViewAnimationListener) }, 0)
-        } else {
-            try {
-//                value_layout!!.visibility = View.GONE
-            } catch (e: Exception) {
-                // ignore
-            }
-            mSettingMode = -1
-        }
-    }
-
-    protected val mSettingHideTask = Runnable { hideSetting(true) }
-
-    /**
-     * callback listener to change camera control values
-     */
-    private val mOnSeekBarChangeListener: OnSeekBarChangeListener = object : OnSeekBarChangeListener {
-        override fun onProgressChanged(seekBar: SeekBar,
-                                       progress: Int, fromUser: Boolean) {
-            if (fromUser) {
-                runOnUiThread(mSettingHideTask, SETTINGS_HIDE_DELAY_MS.toLong())
-            }
-        }
-
-        override fun onStartTrackingTouch(seekBar: SeekBar) {}
-        override fun onStopTrackingTouch(seekBar: SeekBar) {
-            runOnUiThread(mSettingHideTask, SETTINGS_HIDE_DELAY_MS.toLong())
-            if (isActive && checkSupportFlag(mSettingMode)) {
-                when (mSettingMode) {
-                    UVCCamera.PU_BRIGHTNESS, UVCCamera.PU_CONTRAST -> setValue(mSettingMode, seekBar.progress)
-                }
-            } // if (active)
-        }
-    }
-    private val mViewAnimationListener: ViewAnimationListener = object : ViewAnimationListener {
-        override fun onAnimationStart(animator: Animator,
-                                      target: View, animationType: Int) {
-
-//			if (DEBUG) Log.v(TAG, "onAnimationStart:");
-        }
-
-        override fun onAnimationEnd(animator: Animator,
-                                    target: View, animationType: Int) {
-            val id = target.id
-            when (animationType) {
-                ViewAnimationHelper.ANIMATION_FADE_IN, ViewAnimationHelper.ANIMATION_FADE_OUT -> {
-                    val fadeIn = animationType == ViewAnimationHelper.ANIMATION_FADE_IN
-//                    if (id == R.id.value_layout) {
-//                        if (fadeIn) {
-//                            runOnUiThread(mSettingHideTask, SETTINGS_HIDE_DELAY_MS.toLong())
-//                        } else {
-////                            value_layout!!.visibility = View.GONE
-//                            mSettingMode = -1
-//                        }
-//                    } else if (!fadeIn) {
-////					target.setVisibility(View.GONE);
-//                    }
-                }
-            }
-        }
-
-        override fun onAnimationCancel(animator: Animator,
-                                       target: View, animationType: Int) {
-
-//			if (DEBUG) Log.v(TAG, "onAnimationStart:");
-        }
-    }
-
-    //================================================================================
     private val mCPUMonitorTask: Runnable = object : Runnable {
         override fun run() {
-            if (cpuMonitor.sampleCpuUtilization()) {
-                runOnUiThread {
-//                    cpu_load_textview!!.text = String.format(Locale.US, "CPU:%3d/%3d/%3d",
-//                            cpuMonitor.cpuCurrent,
-//                            cpuMonitor.cpuAvg3,
-//                            cpuMonitor.cpuAvgAll)
-                }
-            }
             queueEvent(this, 1000)
         }
     }
+
     private val mFpsTask: Runnable = object : Runnable {
         override fun run() {
-            val srcFps: Float = if (camera_view != null) {
-                camera_view!!.updateFps()
-                camera_view!!.fps
-            } else {
-                0.0f
-            }
-//            val resultFps: Float = if (mImageProcessor != null) {
-//                mImageProcessor!!.updateFps()
-//                mImageProcessor!!.fps
-//            } else {
-//                0.0f
-//            }
-//            fps_textview!!.text = String.format(Locale.US, "FPS:%4.1f->%4.1f", srcFps, resultFps)
             runOnUiThread(this, 1000)
         }
     }
 
     //================================================================================
-    @Volatile
-    private var mIsRunning = false
-    private var mImageProcessorSurfaceId = 0
-
-    /**
-     * start image processing
-     * @param processing_width
-     * @param processing_height
-     */
-    protected fun startImageProcessor(processing_width: Int, processing_height: Int) {
-        if (DEBUG) Log.v(TAG, "startImageProcessor:")
-        mIsRunning = true
-//        if (mImageProcessor == null) {
-//            mImageProcessor = ImageProcessor(PREVIEW_WIDTH, PREVIEW_HEIGHT,  // src size
-//                    MyImageProcessorCallback(processing_width, processing_height)) // processing size
-//            mImageProcessor!!.start(processing_width, processing_height) // processing size
-//            val surface = mImageProcessor!!.surface
-//            mImageProcessorSurfaceId = surface?.hashCode() ?: 0
-//            if (mImageProcessorSurfaceId != 0) {
-//                mCameraHandler!!.addSurface(mImageProcessorSurfaceId, surface, false)
-//            }
-//        }
-    }
-
-    /**
-     * stop image processing
-     */
-    protected fun stopImageProcessor() {
-        if (DEBUG) Log.v(TAG, "stopImageProcessor:")
-        if (mImageProcessorSurfaceId != 0) {
-            mCameraHandler!!.removeSurface(mImageProcessorSurfaceId)
-            mImageProcessorSurfaceId = 0
-        }
-//        if (mImageProcessor != null) {
-//            mImageProcessor!!.release()
-//            mImageProcessor = null
-//        }
-    }
-
-    /**
-     * callback listener from `ImageProcessor`
-     */
-//    private inner class MyImageProcessorCallback(private val width: Int, private val height: Int) : ImageProcessorCallback {
-//        private val matrix = Matrix()
-//        private var mFrame: Bitmap? = null
-//        override fun onFrame(frame: ByteBuffer) {
-//            if (result_view != null) {
-//                val holder = result_view!!.holder
-//                if (holder == null
-//                        || holder.surface == null
-//                        || frame == null) return
-//
-////--------------------------------------------------------------------------------
-//// Using SurfaceView and Bitmap to draw resulted images is inefficient way,
-//// but functions onOpenCV are relatively heavy and expect slower than source
-//// frame rate. So currently just use the way to simply this sample app.
-//// If you want to use much efficient way, try to use as same way as
-//// UVCCamera class use to receive images from UVC camera.
-////--------------------------------------------------------------------------------
-//                if (mFrame == null) {
-//                    mFrame = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-//                    val scaleX = result_view!!.width / width.toFloat()
-//                    val scaleY = result_view!!.height / height.toFloat()
-//                    matrix.reset()
-//                    matrix.postScale(scaleX, scaleY)
-//                }
-//                try {
-//                    frame.clear()
-//                    mFrame!!.copyPixelsFromBuffer(frame)
-//                    val canvas = holder.lockCanvas()
-//                    if (canvas != null) {
-//                        try {
-//                            canvas.drawBitmap(mFrame, matrix, null)
-//                        } catch (e: Exception) {
-//                            Log.w(TAG, e)
-//                        } finally {
-//                            holder.unlockCanvasAndPost(canvas)
-//                        }
-//                    }
-//                } catch (e: Exception) {
-//                    Log.w(TAG, e)
-//                }
-//            }
-//        }
-
-//        override fun onResult(type: Int, result: FloatArray) {
-//            // do something
-//        }
-//
-//    }
 
     companion object {
         private const val DEBUG = true // TODO set false on release
         private const val TAG = "MainActivity"
-
-        /**
-         * set true if you want to record movie using MediaSurfaceEncoder
-         * (writing frame data into Surface camera from MediaCodec
-         * by almost same way as USBCameratest2)
-         * set false if you want to record movie using MediaVideoEncoder
-         */
-        private const val USE_SURFACE_ENCODER = false
 
         /**
          * preview resolution(width)
@@ -559,6 +243,5 @@ class MainActivity : BaseActivity(), CameraDialogParent {
          * 0:YUYV, other:MJPEG
          */
         private const val PREVIEW_MODE = 1
-        private const val SETTINGS_HIDE_DELAY_MS = 2500
     }
 }
